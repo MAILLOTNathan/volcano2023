b67ac8b5419f5c8a904403468f45d49b0c293380
Nathan <nathan.maillot@epitech.eu>
2023-11-25 12:51:54 +0100

    feat: finish json parser

--- lib/game-engine/src/JsonParser/Object.cpp
+++ lib/game-engine/src/JsonParser/Object.cpp
@@ -11,7 +11,7 @@
 
 class Array {
   public:
-    Array();
+    Array(const std::string &input);
     ~Array();
 
   union {
@@ -19,6 +19,7 @@ class Array {
     std::vector<double> numbers;
     std::vector<Object> objects;
     std::vector<Array> arrays;
+    std::vector<bool> booleans;
   } value;
   enum {
     ARRAY,
@@ -26,11 +27,20 @@ class Array {
     OBJECT,
     NUMBER,
   } type;
+  unsigned long size;
+
+  unsigned long addItem(const std::string &input);
+
+  void addBool(const std::string &input);
+  void addString(const std::string &input);
+  void addArray(const std::string &input);
+  void addObject(const std::string &input);
+  void addNumber(const std::string &input);
 };
 
 class Object {
   public:
-    Object(std::string &input);
+    Object(const std::string &input);
     ~Object();
 
   std::string name;
@@ -38,6 +48,7 @@ class Object {
     std::string string;
     double number;
     std::vector<Object> objects;
+    bool boolean;
     Array array;
   } value;
   enum {
@@ -45,6 +56,7 @@ class Object {
     STRING,
     OBJECT,
     NUMBER,
+    BOOL,
   } type;
   unsigned long size;
 
@@ -56,17 +68,38 @@ class Object {
   unsigned long setArray(const std::string &input);
 };
 
-Object::Object(std::string &input)
+Array::Array(const std::string &input)
 {
-  for (this->size = 0; input[this->size]; this->size++) {
-    input = input.substr(this->size);
-    switch (input.front()) {
-      case '{':
-        this->addObject(input);
+  for (this->size = 1; input[this->size] && input[this->size] != ','; this->size++) {
+    switch (input[this->size]) {
       case '"':
-        this->setName(input);
+        this->addString(input.substr(this->size));
+      case '{':
+        this->addObject(input.substr(this->size));
+      case '[':
+        this->addArray(input.substr(this->size));
+      case 't':
+        this->addBool(input.substr(this->size));
+      case 'f':
+        this->addBool(input.substr(this->size));
+      default:
+        this->addNumber(input.substr(this->size));
+    }
+  }
+}
+
+Object::Object(const std::string &input)
+{
+  std::string text = input;
+  for (this->size = 0; text[this->size]; this->size++) {
+    text = text.substr(this->size);
+    switch (text.front()) {
+      case '{':
+        this->addObject(text);
+      case '"':
+        this->setName(text);
       case ':':
-        this->addProperty(input);
+        this->addProperty(text);
       default:
         break;
     }
@@ -98,16 +131,109 @@ unsigned long Object::setString(const std::string &input)
   return written;
 }
 
+bool isDigit(char c)
+{
+  return (c >= '0' && c <= '9') || c == '.';
+}
+
+std::string getNumberString(const std::string &string)
+{
+  std::string number;
+  bool point = false;
+
+  for (unsigned long i = 0; string[i] && isDigit(string[i]); i++) {
+    point = !point && string[i] == '.' ? true : false;
+    if (point && string[i] == '.') {
+      std::cout << "JSON_PARSING_ERROR: Invalid number." << std::endl;
+      return number;
+    }
+    number += string[i];
+  }
+  return number;
+}
+
+void Array::addNumber(const std::string &input)
+{
+  std::string number = getNumberString(input);
+  double value = std::atof(number.c_str());
+
+  this->size += number.length();
+  this->value.numbers.push_back(value);
+}
+
+void Array::addString(const std::string &input)
+{
+  this->size++;
+
+  std::string value;
+
+  for (unsigned int i = 1; input[i] && input[i] != '"'; i++, this->size++) {
+    value += input[i];
+  }
+  this->size++;
+  this->value.strings.push_back(value);
+}
+
+void Array::addObject(const std::string &input)
+{
+  Object *object = new Object(input);
+
+  this->value.objects.push_back((*object));
+  this->size += object->size;
+}
+
+void Array::addArray(const std::string &input)
+{
+  Array *array = new Array(input);
+  this->value.arrays.push_back((*array));
+}
+
+unsigned int getBool(const std::string &input)
+{
+  std::string trueValue = "true";
+
+  if (input.compare(0, trueValue.length(), input)) {
+    return true;
+  }
+  return false;
+}
+
+void Array::addBool(const std::string &input)
+{
+  bool value = getBool(input);
+
+  this->size += value ? 4 : 5;
+  this->value.booleans.push_back(value);
+}
+
+unsigned long Array::addItem(const std::string &input)
+{
+  for (this->size = 1; input[this->size] && input[this->size] != ','; this->size++) {
+    switch (input[this->size]) {
+      case '"':
+        this->addString(input.substr(this->size));
+      case '{':
+        this->addObject(input.substr(this->size));
+      case '[':
+        this->addArray(input.substr(this->size));
+      case 't':
+        this->addBool(input.substr(this->size));
+      case 'f':
+        this->addBool(input.substr(this->size));
+      default:
+        this->addNumber(input.substr(this->size));
+    }
+  }
+}
+
 unsigned long Object::setArray(const std::string &input)
 {
   std::string text = input.substr(1);
   unsigned long written = 0;
   this->size++;
 
-  for (unsigned int i = 0; text[i] && text[i] != ']'; i++) {
-    this->value.string += text[i];
-    this->size++;
-    written++;
+  for (unsigned long i = 0; text[i] && text[i] != ']'; i++) {
+    i += this->value.array.addItem(text.substr(i));
   }
   return written;
 }
